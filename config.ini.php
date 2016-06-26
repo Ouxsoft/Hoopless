b70bad071b89282cf267d380286f4ff5aae48329
mrheroux <git@mrheroux.com>
2016-06-26 22:52:48 +0000

    Add files via upload

---
+++ config.ini.php
@@ -0,0 +1,21 @@
+;<?php return; ?>
+[SERVER]
+server = INSERT
+timezone = INSERT 
+debug_ip = INSERT
+
+[DATABASE]
+host = INSERT
+user = INSERT
+password = INSERT
+dbname = INSERT
+
+[ADMIN]
+company = INSERT
+email = INSERT
+phone = INSERT
+fax = INSERT
+
+[MISC]
+abbreviation =  INSERT
+theme = INSERT

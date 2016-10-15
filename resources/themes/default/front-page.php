cce72983a955e9bda4082a282b72b8d03e195e2b
mheroux <contact@mrheroux.com>
2016-10-15 19:55:10 -0400

    Button verbage

--- resources/themes/default/front-page.php
+++ resources/themes/default/front-page.php
@@ -18,7 +18,7 @@ echo "<li>maintain college web services;</li>";
 echo "<li>et al.</li>";
 echo "</ul>";
 echo "<p>If you are a business seeking a web-based solution or an employer looking to hire, feel free to get in touch with me.</p>";
-echo "<br/><p><button type=\"button\" class=\"btn btn-primary btn-lg\" onclick=\"window.location.href='{$instance->href("contact.html")}'\">I'm looking to hire<span class=\"glyphicon glyphicon-menu-right\"></span></button> <button type=\"button\" class=\"btn btn-default btn-lg\" onclick=\"window.location.href='{$instance->href("contact.html")}'\">I need a website <span class=\"glyphicon glyphicon-menu-right\"></span></button></p>";
+echo "<br/><p><button type=\"button\" class=\"btn btn-primary btn-lg\" onclick=\"window.location.href='{$instance->href("contact.html")}'\">I'm looking to hire<span class=\"glyphicon glyphicon-menu-right\"></span></button> <button type=\"button\" class=\"btn btn-default btn-lg\" onclick=\"window.location.href='{$instance->href("contact.html")}'\">I need a web-solution <span class=\"glyphicon glyphicon-menu-right\"></span></button></p>";
 echo "</div>";
 echo "</div>";
 echo "</div>";

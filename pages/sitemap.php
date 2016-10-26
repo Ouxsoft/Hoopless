5a718bb834acfc59de88f88b211795d3a1dd88c7
mheroux <contact@mrheroux.com>
2016-10-25 20:08:33 -0400

    Add header to process XML correctly

--- pages/sitemap.php
+++ pages/sitemap.php
@@ -1,4 +1,6 @@
 <?php
+header("Content-type: text/xml");
+
 echo '<?xml version="1.0" encoding="UTF-8"?>';
 echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
 $results = $db->query('SELECT `link`,`change_freq`,`priority` FROM `pages`;');

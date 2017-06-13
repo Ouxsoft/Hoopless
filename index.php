0e5768abe20ff69e871c3daa1d7241a32185a8de
mrheroux <contact@mrheroux.com>
2017-06-12 20:31:34 -0400

    Style update

--- index.php
+++ index.php
@@ -1,21 +1,91 @@
 <?php
 session_start();
 
-require 'includes/log.inc';
 require 'includes/db.inc';
 require 'includes/instance_controller.inc';
 
 require 'includes/Mustache/Autoloader.php';
 Mustache_Autoloader::register();
 
-$view = new Mustache_Engine(array(
-  'loader'=> new Mustache_Loader_CascadingLoader(
-    array(
-	     new Mustache_Loader_FilesystemLoader('resources/themes/'.$instance->website['theme'].'/templates'),
-       new Mustache_Loader_FilesystemLoader('nodes/'.$instance->page['current']['node_id']),
-       )
-    )
-  )
+/*
+// use .html instead of .mustache for default template extension
+$options =  array('extension' => '.html');
+
+$m = new Mustache_Engine(array(
+    'loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__) . '/views', $options),
+));
+*/
+
+$view = new Mustache_Engine(
+	array(
+		'loader'=> new Mustache_Loader_CascadingLoader(
+			array(
+				new Mustache_Loader_FilesystemLoader('resources/themes/'.$instance->website['theme'].'/templates'),
+				new Mustache_Loader_FilesystemLoader('nodes/'.$instance->page['current']['node_id']),
+			)
+		)
+	)
+);
+
+
+// allow for mustache permalinks
+// q=2&b=2&3#top
+$instance->link = array(
+	'id' => function($string, Mustache_LambdaHelper $helper) {
+		global $instance;
+		// separate fragment identifier from remaining string
+		if (($pos = strpos($string, '#')) !== FALSE) {
+			list($string, $fragment_id) = explode('#',$string);
+		}
+	
+		// split alias_id and remaining string
+		list($alias_id, $string) = explode('&',$string,2);
+	
+		// split parameters from remaining string
+		parse_str($string, $parameters);
+
+	// make href identifier for caching
+		$id = $alias_id.'?'.$parameters['q'];
+		$address = '';
+				
+		// will have to add something spiffy to accomidate for no record_id but extra and visa versa
+		if(array_key_exists($id, $instance->cache['href'])){
+			// return cached
+			// need to build out remaining q
+			return $instance->cache['href'][$id].'#'.$fragment_id;
+			// make smarter this if makes no sense
+		} else {
+			// look up and cache
+			global $db;
+			$db->bind('alias_id',$alias_id);
+			$row = $db->row('SELECT `node_id`,`alias` FROM `node_alias` WHERE `alias_id` = :alias_id LIMIT 1;');
+			$node_id = $row['node_id'];
+			$alias = $row['alias'];
+
+			// PAGE?q=2&sa=r123GFESFT
+			if(isset($parameters['q'])){
+				$parameters['q'] = $helper->render($parameters['q']);
+				list(,,,$salt,$hash) = explode('$',crypt($node_id,'$6$rounds=5000$'.md5($parameters['q'].$instance->config['href_salt'].'$')));
+				$sa = urlencode($salt.$hash);
+				$instance->cache['href'][$id] = array(
+					'alias' => $alias,
+					'q' => $parameters['q'],
+					'sa' => $sa,
+				);
+				$bool = false;
+				$output = $alias;
+				foreach($parameters as $key => $value){
+					$output .= (($bool)?'&amp;':'?').$key.'='.$value;
+					$bool = true;
+				}
+				$output .= '&amp;sa='.$sa.'#'.$fragment_id;
+				return $output;
+			} else {
+				$instance->cache['href'][$id] = $alias;
+				return $alias.$extra;
+			}
+		}
+	}
 );
 
 echo $view->render('header',$instance);
@@ -31,9 +101,9 @@ switch ($instance->page['current']['state']) {
 			$alert->add('alert','<a href="'.$instance->href('users/sign-up').'">Sign up</a> to access this page');
 			$alert->get();
 			echo '</div>';
-      echo $view->render('sign-in',$instance);
+      		echo $view->render('sign-in',$instance);
 		} else {
-  		include('nodes/'.$instance->page['current']['node_id'].'/logic.php');
+	  		include('nodes/'.$instance->page['current']['node_id'].'/logic.php');
 			echo $view->render('view',$instance);
 		}
 		break;
--- nodes/35/logic.php
+++ nodes/35/logic.php
@@ -3,7 +3,7 @@ include 'includes/alert.inc';
 
 // add version history
 if($instance->verify()){
-	$file = 'nodes/'.$record_id.'.php';
+	$file = 'nodes/'.$record_id.'/view.mustache';
 	if(!file_exists($file)){
 		echo 'File does not exists';
 		die();
@@ -16,46 +16,8 @@ if($instance->verify()){
 		fwrite($fp, $data) or die(print_r(error_get_last(),true));
 		fclose($fp);
 	}
-
+	// replace with mustache call build alert into class
 	$alert->get();
 
-	echo '<div class="container background-white">';
-	echo '<form method="post" id="edit" name="edit" enctype="multipart/form-data">';
-	echo '<select>';
-	foreach(array("WYSIWYG","PHP Code") as $key => $value) {
-		echo '<option>'.$value.'</option>';
-	}
-	echo '</select>';
-	echo '<div class="text-right">';
-	echo '<button type="submit" name="submit" class="btn btn-default" form="edit" value="save"><span class="glyphicon glyphicon-floppy-disk" aria-hidden="true"></span> Save</button>';
-	echo '</div>';
-	echo '<textarea name="content" wrap="soft">'.htmlspecialchars(file_get_contents($file)).'</textarea>';
-	echo '<input type="hidden" name="submit_check" value="1">';
-	echo '</form>';
-	echo '</div>';
-	echo '<script src="'.SERVER.'/assets/tinymce/tinymce.min.js"></script>';
-	echo '<script>';
-	echo 'tinymce.init({ ';
-	echo 'protect: [ ';
-	echo '/<\?php[\s\S]*?\?>/g ';
-	echo '], ';
-	echo 'selector: \'textarea\', ';
-	echo 'height: 500, ';
-	echo 'theme: \'modern\', ';
-	echo 'plugins: [ ';
-	echo '\'advlist autolink lists link image charmap print preview hr anchor pagebreak\', \'searchreplace wordcount visualblocks visualchars code fullscreen\', \'insertdatetime media nonbreaking save table contextmenu directionality\', \'template paste textcolor colorpicker textpattern imagetools codesample\' ';
-	echo '], ';
-	echo 'toolbar1: \'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image\', ';
-	echo 'toolbar2: \'print preview media | forecolor backcolor emoticons | codesample\', ';
-	echo 'image_advtab: true, ';
-	echo 'templates: [ ';
-	echo '{ title: \'Test template 1\', content: \'Test 1\' }, ';
-	echo '{ title: \'Test template 2\', content: \'Test 2\' } ';
-	echo '], ';
-//	echo 'content_css: [ ';
-//	echo '\'//fonts.googleapis.com/css?family=Lato:300,300i,400,400i\', ';
-//	echo '\'//www.tinymce.com/css/codepen.min.css\' ';
-//	echo '] ';
-	echo '}); ';
-	echo '</script>';
+	$instance->render['file'] = htmlspecialchars(file_get_contents($file));
 }

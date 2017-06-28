90cc9114bdf57cb71a639d62106553ba23d7a1b0
mrheroux <contact@mrheroux.com>
2017-06-27 21:57:02 -0400

    Fix star rating

--- nodes/12/logic.php
+++ nodes/12/logic.php
@@ -6,26 +6,26 @@ function parse_stars($json) {
 			'title' => $row['title'],
 			'score' => array(),
 		);
-		$count = 0;
+		$count = 1;
 		do {
-			if ($row['score'] > 2) {
+			if ($row['score'] >= 2 ) {
 				$sub_array['score'][] = 'star';
-			} else if ($row['score'] == 1) {
+			} else if ($row['score'] >= 1) {
 				$sub_array['score'][] = 'star_half';
 			} else {
 				$sub_array['score'][] = 'star_border';
 			} 
 			$row['score'] = $row['score'] - 2;
 			$count = $count + 2;
-		} while ($count < 10);
+		} while ($count <= 10);
 		$array[] = $sub_array;
 	}
-	return $array;	
+	return $array;
 }
 
 $json_file = file_get_contents('nodes/' . $instance->page['current']['node_id']. '/data.json');
 $instance->render['data'] = json_decode($json_file, true);
-$instance->render['data']['skills'] = parse_stars($instance->render['data']['skills']);
-$instance->render['data']['tools'] = parse_stars($instance->render['data']['tools']);
-$instance->render['data']['languages'] = parse_stars($instance->render['data']['languages']);
+foreach(array('skills','tools','languages') as $value){
+	$instance->render['data'][$value] = parse_stars($instance->render['data'][$value]);
+}
 ?>

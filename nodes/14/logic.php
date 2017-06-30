8588927538fe5e51ffc89fc4f7d49954ec5de74c
mheroux <meyeah@you.com>
2017-06-29 21:08:55 -0400

    implemented json for robotic page

--- nodes/14/logic.php
+++ nodes/14/logic.php
@@ -1,100 +1,31 @@
 <?php
 //nihil sub sole novum
-
-function portfolio_href($type, $string){
-	// get href for portfolio resources or href provided
-	if(substr($string, 0, 4) === "http"){
-		return($string);
-	} else {
-		return SERVER.'/assets/portfolio/'.$type.'/'.$string;
+function parse_stars($json) {
+	foreach($json as $row){
+		$sub_array = array(
+			'title' => $row['title'],
+			'score' => array(),
+		);
+		$count = 1;
+		do {
+			if ($row['score'] >= 2 ) {
+				$sub_array['score'][] = 'star';
+			} else if ($row['score'] >= 1) {
+				$sub_array['score'][] = 'star_half';
+			} else {
+				$sub_array['score'][] = 'star_border';
+			} 
+			$row['score'] = $row['score'] - 2;
+			$count = $count + 2;
+		} while ($count <= 10);
+		$array[] = $sub_array;
 	}
+	return $array;
 }
 
-
-echo '<div class="col-md-12 mb-2 bg-1">';
-echo "<h2 class=\"center\">Experiences</h2>";
-echo "<h3>Careers</h3>";
-
-echo "<div class=\"row\">";
-echo "<div class=\"col-md-4\"><h4>Questech&reg; Corporation</h4>2006 to 2011</div>";
-echo "<div class=\"col-md-8\"><h4>Programmer Operator</h4>";
-echo "<p>Developed software applications for Fanuc RJ32 robots and operated robots.</a></p>";
-echo "<div class=\"experience-details\">";
-echo "<span class=\"glyphicon glyphicon-map-marker\"></span>Rutland, Vermont USA | <span class=\"glyphicon glyphicon-link\"></span> <a href=\"http://questech.com/\">http://questech.com/</a>";
-echo "</div>";
-echo "</div>";
-echo "</div>";
-echo "</div>";
-
-// introduction
-echo "<div class=\"col-md-12 alert alert-info\" role=\"alert\"><b>Fun fact</b>: Did you know my grandfather, the late <b>Norman Heroux</b>, invented the <em>first</em> successful industrial robot? <a href=\"https://en.wikipedia.org/wiki/Industrial_robot\"><span class=\"glyphicon glyphicon-link\"></span> Read more</a>.</div>";
-
-// abilities
-echo '<div class="col-md-12 mb-2 bg-1">';
-echo "<h2 class=\"center\">Abilities</h2>";
-
-// skills
-$results = $db->query("SELECT CONCAT(`name`, IF(`started` <= date_sub(now(), interval 10 year), ' (10+ Years)', IF(`started` < date_sub(now(), interval -10 year), CONCAT(' (',TIMESTAMPDIFF(YEAR,`started`, CURDATE()), ' Years)'), ''))) AS `title`, `score` FROM `abilities` WHERE `category` = 'Robotic skills' ORDER BY `score` DESC, `name`");
-if(count($results)>0){
-	echo "<h3>Skills</h3>";
-	echo "<hr/>";
-	echo "<div class=\"row\">";
-	foreach($results as $row) {
-		echo "<div class=\"col-md-6\">";
-		echo "<ul class=\"no-bullets\">";
-		echo "<li>";
-		echo "<span class=\"ability-title\">{$row["title"]}</span>";
-		echo "<span class=\"ability-score\">";
-		for($i = 1; $i<=5; $i++){
-			echo "<span class=\"glyphicon glyphicon-star".(($i<=$row["score"])?" filled":"")."\"></span>";
-		}
-		echo "</span>";
-		echo "</li>";
-		echo "</div>";
-	}
-	echo "</div>";
+$json_file = file_get_contents('nodes/' . $instance->page['current']['node_id']. '/data.json');
+$instance->render['data'] = json_decode($json_file, true);
+foreach(array('skills','tools','languages') as $value){
+	$instance->render['data'][$value] = parse_stars($instance->render['data'][$value]);
 }
-
-// tools
-$results = $db->query("SELECT CONCAT(`name`, IF(`started` <= date_sub(now(), interval 10 year), ' (10+ Years)', IF(`started` < date_sub(now(), interval -10 year), CONCAT(' (',TIMESTAMPDIFF(YEAR,`started`, CURDATE()), ' Years)'), ''))) AS `title`, `score` FROM `abilities` WHERE `category` = 'Robotic tools' ORDER BY `score` DESC, `name`");
-if(count($results)>0){
-	echo "<h3>Tools</h3>";
-	echo "<hr/>";
-	echo "<div class=\"row\">";
-	foreach($results as $row) {
-		echo "<div class=\"col-md-6\">";
-		echo "<ul class=\"no-bullets\">";
-		echo "<li>";
-		echo "<span class=\"ability-title\">{$row["title"]}</span>";
-		echo "<span class=\"ability-score\">";
-		for($i = 1; $i<=5; $i++){
-			echo "<span class=\"glyphicon glyphicon-star".(($i<=$row["score"])?" filled":"")."\"></span>";
-		}
-		echo "</span>";
-		echo "</li>";
-		echo "</div>";
-	}
-	echo "</div>";
-}
-echo "</div>";
-
-
-
-// projects
-echo '<div class="col-md-12 mb-2 bg-1">';
-
-$db->bind("category","robot");
-$results = $db->query("SELECT CONCAT(IF(`title` IS NULL, 'N/A', CONCAT('<b>',`title`,'</b>')), IF(`media` IS NULL, '', CONCAT(' - ',`media`)),' ',`year`) AS `title`, `href`,`thumbnail` FROM `portfolio` WHERE `category` = :category ORDER BY `year` DESC, `id` ASC");
-if(count($results)>0){
-	echo "<h2 class=\"center\">Projects</h2>";
-	echo "<p>Presented below are discussions and images of systems I have designed for robots. </p>";
-	echo "<div class=\"row\">";
-	foreach ($results as $row){
-		echo "<div class=\"col-xs-3\">";
-		echo "<div class=\"thumbnail\"><a href=\"".portfolio_href("images", $row["href"])."\" alt=\"{$row["title"]}\" data-toggle=\"lightbox\" data-gallery=\"multiimages\" data-title=\"{$row["title"]}\"><img src=\"".portfolio_href("thumbnails", $row["thumbnail"])."\" style=\"\"/></a></div>";
-		echo "</div>";
-	}
-	echo "</div>";
-}
-echo "</div>";
 ?>

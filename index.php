881f09f53e0f20f0f56e3cbb05938869caa69a9f
mrheroux <contact@mrheroux.com>
2017-08-13 21:07:45 -0400

    Separating User from Instance; adding node editor;

--- index.php
+++ index.php
@@ -3,100 +3,114 @@ session_start();
 
 require 'includes/db.inc';
 require 'includes/instance_controller.inc';
+require 'includes/user.inc';
 require 'includes/Mustache/Autoloader.php';
 Mustache_Autoloader::register();
 
-// process nodes logic
-if($instance->user['permission']==true){
-	include 'nodes/'.$instance->page['current']['node_id'].'/logic.php';
-}
+$user = new user();
+$instance = new instance_controller();
+$user->get();
 
-// mustache functions (f)
-$instance->f = array(
-	// date, e.g. {{#f.date}}Y{{/f.date}}
-	'date' => function($format) {
-		return date($format);
-	},
-	
-	'get_menu' => function($menu) {
-		global $db;
-		global $instance;
-		$db->bind('menu',$menu);
-		$results = $db->query('SELECT `menu_item`.`node_id` AS `id`,`menu_item`.`parent_id`,`menu_item`.`title`, `node_alias`.`alias` FROM `menu` LEFT JOIN `menu_item` ON `menu`.`menu_id` = `menu_item`.`menu_id` LEFT JOIN `node_alias` ON `menu_item`.`node_id` = `node_alias`.`node_id` WHERE `menu`.`title` = :menu ORDER BY `menu_item`.`parent_id`,`menu_item`.`weight` DESC, `menu_item`.`title` ASC');
-		$instance->menu[$menu] = $instance->build_menu($instance->build_tree($results),0);
-		return;
-	},
-	
-	// permalinks, e.g. {{#f.link}}33&q=2&a=1&b=2&c=3#top{{/f.link}}
-	'link' => function($string, Mustache_LambdaHelper $helper) {
-		global $instance;
-	
-		// separate fragment identifier from remaining string
-		if (($pos = strpos($string, '#')) !== FALSE) {
-			list($string, $fragment_id) = explode('#',$string);
-		}
-		// split alias_id and remaining string
-		list($alias_id, $string) = explode('&',$string,2);
-		// split parameters from remaining string
-		parse_str($string, $parameters);
-		// make href identifier for caching
-		$id = $alias_id.'?'.$parameters['q'];
-		$address = '';
-		// will have to add something spiffy to accomidate for no record_id but extra and visa versa
-		if(array_key_exists($id, $instance->cache['href'])){
-			// return cached need to build out remaining q
-			return $instance->cache['href'][$id].'#'.$fragment_id;
-			// make smarter this if makes no sense
-		} else {
-			// look up and cache
+switch ($instance->page['action']){
+	case 'edit':
+		include 'includes/node_editor.inc';
+		break;
+	case 'view':
+
+	// process nodes logic
+	if($instance->user['permission']==true){
+		include 'nodes/'.$instance->page['current']['node_id'].'/logic.php';
+	}
+
+	// mustache functions (f)
+	$instance->f = array(
+		// date, e.g. {{#f.date}}Y{{/f.date}}
+		'date' => function($format) {
+			return date($format);
+		},
+
+		'get_menu' => function($menu) {
 			global $db;
-			$db->bind('alias_id',$alias_id);
-			$row = $db->row('SELECT `node_id`,`alias` FROM `node_alias` WHERE `alias_id` = :alias_id LIMIT 1;');
-			$node_id = $row['node_id'];
-			$alias = $row['alias'];
+			global $instance;
+			$db->bind('menu',$menu);
+			$results = $db->query('SELECT `menu_item`.`node_id` AS `id`,`menu_item`.`parent_id`,`menu_item`.`title`, `node_alias`.`alias` FROM `menu` LEFT JOIN `menu_item` ON `menu`.`menu_id` = `menu_item`.`menu_id` LEFT JOIN `node_alias` ON `menu_item`.`node_id` = `node_alias`.`node_id` WHERE `menu`.`title` = :menu ORDER BY `menu_item`.`parent_id`,`menu_item`.`weight` DESC, `menu_item`.`title` ASC');
+			$instance->menu[$menu] = $instance->build_menu($instance->build_tree($results),0);
+			return;
+		},
 
-			// PAGE?q=2&sa=r123GFESFT
-			if(isset($parameters['q'])){
-				$parameters['q'] = $helper->render($parameters['q']);
-				list(,,,$salt,$hash) = explode('$',crypt($node_id,'$6$rounds=5000$'.md5($parameters['q'].$instance->config['href_salt'].'$')));
-				$sa = urlencode($salt.$hash);
-				$instance->cache['href'][$id] = array(
-					'alias' => $alias,
-					'q' => $parameters['q'],
-					'sa' => $sa,
-				);
-				$bool = false;
-				$output = $alias;
-				foreach($parameters as $key => $value){
-					$output .= (($bool)?'&amp;':'?').$key.'='.$value;
-					$bool = true;
-				}
-				$output .= '&amp;sa='.$sa.'#'.$fragment_id;
-				return $output;
+		// permalinks, e.g. {{#f.link}}33&q=2&a=1&b=2&c=3#top{{/f.link}}
+		'link' => function($string, Mustache_LambdaHelper $helper) {
+			global $instance;
+
+			// separate fragment identifier from remaining string
+			if (($pos = strpos($string, '#')) !== FALSE) {
+				list($string, $fragment_id) = explode('#',$string);
+			}
+			// split alias_id and remaining string
+			list($alias_id, $string) = explode('&',$string,2);
+			// split parameters from remaining string
+			parse_str($string, $parameters);
+			// make href identifier for caching
+			$id = $alias_id.'?'.$parameters['q'];
+			$address = '';
+			// will have to add something spiffy to accomidate for no record_id but extra and visa versa
+			if(array_key_exists($id, $instance->cache['href'])){
+				// return cached need to build out remaining q
+				return $instance->cache['href'][$id].'#'.$fragment_id;
+				// make smarter this if makes no sense
 			} else {
-				$instance->cache['href'][$id] = $alias;
-				return $alias.$extra;
+				// look up and cache
+				global $db;
+				$db->bind('alias_id',$alias_id);
+				$row = $db->row('SELECT `node_id`,`alias` FROM `node_alias` WHERE `alias_id` = :alias_id LIMIT 1;');
+				$node_id = $row['node_id'];
+				$alias = $row['alias'];
+
+				// PAGE?q=2&sa=r123GFESFT
+				if(isset($parameters['q'])){
+					$parameters['q'] = $helper->render($parameters['q']);
+					list(,,,$salt,$hash) = explode('$',crypt($node_id,'$6$rounds=5000$'.md5($parameters['q'].$instance->config['href_salt'].'$')));
+					$sa = urlencode($salt.$hash);
+					$instance->cache['href'][$id] = array(
+						'alias' => $alias,
+						'q' => $parameters['q'],
+						'sa' => $sa,
+					);
+					$bool = false;
+					$output = $alias;
+					foreach($parameters as $key => $value){
+						$output .= (($bool)?'&amp;':'?').$key.'='.$value;
+						$bool = true;
+					}
+					$output .= '&amp;sa='.$sa.'#'.$fragment_id;
+					return $output;
+				} else {
+					$instance->cache['href'][$id] = $alias;
+					return $alias.$extra;
+				}
 			}
 		}
-	}
-	
-);
 
-$html = new Mustache_Engine(
-	array(
-		'loader'=> new Mustache_Loader_CascadingLoader(
-			array(
-				new Mustache_Loader_FilesystemLoader('themes/'.$instance->website['theme'].'/templates'),
-				new Mustache_Loader_FilesystemLoader('themes/'.$instance->website['theme'].'/templates/partials'),
-				new Mustache_Loader_FilesystemLoader('nodes/'.$instance->page['current']['node_id']),
+	);
+
+	$html = new Mustache_Engine(
+		array(
+			'loader'=> new Mustache_Loader_CascadingLoader(
+				array(
+					new Mustache_Loader_FilesystemLoader('themes/'.$instance->website['theme'].'/templates'),
+					new Mustache_Loader_FilesystemLoader('themes/'.$instance->website['theme'].'/templates/partials'),
+					new Mustache_Loader_FilesystemLoader('nodes/'.$instance->page['current']['node_id']),
+				)
 			)
 		)
-	)
-);
+	);
 
-if ($instance->page['current']['standalone']==1){
-	echo $html->render('view',$instance);
-} else {
-	echo $html->render('default',$instance);
+	if ($instance->page['current']['standalone']==1){
+		echo $html->render('view',$instance);
+	} else {
+		echo $html->render('default',$instance);
+	}
+		break;
 }
+
 ?>
--- nodes/37/logic.php
+++ nodes/37/logic.php
@@ -19,54 +19,55 @@ class PDF extends FPDF
 			$this->Cell($content_width, 5,$data->email,0,0,'R',0,$data->email);
 			$this->ln();
 			// print thick line
-			$this->ThickLine1();
+			$this->ThickLine('up');
 		} else {
-	    	// Move to the right
-	    	$this->SetFont('San','B',16);
-	    	$this->Cell($content_width/2,6,$data->name,0,0,'L');
-	    	
+    	// Move to the right
+    	$this->SetFont('San','B',16);
+    	$this->Cell($content_width/2,6,$data->name,0,0,'L');
+
 			$this->SetFont('PragatiNarrow','',10);
 			$this->Cell($content_width/2,6,'Page '.$this->pagecount,0,0,'R');
 			$this->ln();
-			$this->ThickLine1();
+			$this->ThickLine('up');
 		}
 	}
-	function ThickLine1(){
+	function ThickLine($direction = 'up', $width = 100){
 		global $content_width;
 		// print line
-		$x = $this->GetX();
+		$x = $this->GetX() + ($content_width * (1-$width/100))/2;
 		$y = $this->GetY();
-		$this->SetFillColor(0,0,0);
-		$this->Rect($x, $y, $content_width, 0.61, F);
-		$this->SetFillColor(96,96,96);
-		$this->Rect($x, $y+0.61, $content_width, 1, F);
-		$this->SetFillColor(192,192,192);
-		$this->Rect($x, $y+1.61, $content_width, 0.61, F);
-		$this->ln();
-	}
-	function ThickLine2(){
-		global $content_width;
-		// print line
-		$x = $this->GetX();
-		$y = $this->GetY();
-		$this->SetFillColor(192,192,192);
-		$this->Rect($x, $y, $content_width, 0.61, F);
-		$this->SetFillColor(96,96,96);
-		$this->Rect($x, $y+0.61, $content_width, 1, F);
-		$this->SetFillColor(0,0,0);
-		$this->Rect($x, $y+1.61, $content_width, 0.61, F);
-		$this->ln();
+		$this->ln(1.71);
+		$width = $content_width * $width/100;
+		switch ($direction) {
+			case 'up':
+				$this->SetFillColor(0,0,0);
+				$this->Rect($x, $y, $width, 0.61, F);
+				$this->SetFillColor(96,96,96);
+				$this->Rect($x, $y+0.61, $width, 1, F);
+				$this->SetFillColor(192,192,192);
+				$this->Rect($x, $y+1.61, $width, 0.61, F);
+				break;
+			case 'down':
+				$this->SetFillColor(192,192,192);
+				$this->Rect($x, $y, $width, 0.61, F);
+				$this->SetFillColor(96,96,96);
+				$this->Rect($x, $y+0.61, $width, 1, F);
+				$this->SetFillColor(0,0,0);
+				$this->Rect($x, $y+1.61, $width, 0.61, F);
+				break;
+			}
+		$this->ln(1.71);
 	}
 }
 
 // print awards
-function li($indent_amount, $char, $value, $date = NULL){
+function li($indent_amount, $char, $array, $date = NULL){
 	global $pdf;
 	global $indent;
 	global $content_width;
 	global $column_width;
 	global $margin;
-	
+
 	// print bullet
 	if ($char == NULL) {
 		$char = chr(149);
@@ -79,12 +80,12 @@ function li($indent_amount, $char, $value, $date = NULL){
 	$indent_total = $indent*$indent_amount+$margin['left'];
 	$pdf->SetX($indent_total);
 	if($date == NULL){
-		$pdf->MultiCell(0, 4,iconv('UTF-8', 'windows-1252', $value),0,'J',false);
+		$pdf->MultiCell(0, 4,iconv('UTF-8', 'windows-1252', $array),0,'J',false);
 	} else {
 		$column_width2 = ($content_width-$indent*$indent_amount)/2;
-		
+
 		$pdf->SetFont('PragatiNarrow','B',10);
-		$pdf->Cell($column_width2,4,iconv('UTF-8', 'windows-1252', $value),'',0,'L');
+		$pdf->Cell($column_width2,4,iconv('UTF-8', 'windows-1252', $array),'',0,'L');
 		$pdf->SetFont('PragatiNarrow','',10);
 		$pdf->Cell($column_width2,4,$date,'',0,'R');
 		$pdf->ln();
@@ -108,7 +109,13 @@ $column_width = $content_width/3;
 $indent = 10;
 
 // decode json file
-$json_file = file_get_contents('nodes/' . $instance->page['current']['node_id']. '/data.json');
+if(isset($_GET['resume'])){
+	$data_file = preg_replace("/[^A-Za-z0-9 -]/", '', $_GET['resume']);
+} else {
+	$data_file = 'default';
+}
+
+$json_file = file_get_contents('nodes/' . $instance->page['current']['node_id']. '/'. $data_file .'.json');
 $data = json_decode($json_file);
 
 // init page
@@ -116,50 +123,61 @@ $pdf->SetMargins($margin['left'], $margin['top'],$margin['right']);
 $pdf->AddPage();
 
 // print objective
-$pdf->SetFont('San','B',12);
-$pdf->MultiCell($content_width, 8,'OBJECTIVE:',0,'L');
-$pdf->SetFont('PragatiNarrow','',10);
-$pdf->MultiCell($content_width, 5,$data->objective,0,'C');
-$pdf->ln(1);
-$pdf->ThickLine2();
-
-
-// print skills
-$pdf->SetFont('San','B',12);
-$pdf->MultiCell($content_width, 8,'SKILLS:',0,'L');
-$pdf->SetFont('PragatiNarrow','',10);
-foreach($data->skills as $key => $value){
-	//li($indent_amount, NULL, $value);
-    $pdf->Cell($indent,4,chr(149),'',0,'R');
-	$pdf->Cell($column_width-$indent,4,$value,'',0,'L');
-	if($pdf->GetX() > $column_width*3) {
-		$pdf->ln();
-	}
+if(isset($_GET['objective'])){
+	$objective = preg_replace("/[^A-Za-z0-9 ]/", '', $_GET['objective']);
+} else {
+	$objective = $data->objective;
 }
 
-// print misc.
-foreach($data->other as $key => $value){
-	$pdf->SetFont('San','B',12);
-	$pdf->MultiCell($content_width, 7,strtoupper($value->title).':',0,'L');
+$pdf->SetFont('San','B',12);
+$pdf->MultiCell($content_width, 6,'OBJECTIVE:  '.$objective,0,'C');
+//$pdf->ThickLine('down',100-61.8);
 
-	foreach($value->list as $key2 => $value2){
-		$pdf->SetFont('PragatiNarrow','B',11);
-		li(1, ' ', $value2->title,$value2->date);
-		$pdf->SetFont('PragatiNarrow','I',8.5);
-		li(1, ' ', $value2->description);
-		$pdf->ln(0.4);
-		foreach($value2->list as $key3 => $value3){
-			$pdf->SetFont('PragatiNarrow','B',10);
-			li(2, NULL,  $value3->title);
-			foreach($value3->list as $key4 => $value4){
-				$pdf->SetFont('PragatiNarrow','',10);
-				li(3, '—', $value4);
+// objective line 2
+$pdf->SetFont('PragatiNarrow','B',12);
+$pdf->MultiCell($content_width, 5,$data->description,0,'C');
+$pdf->ThickLine('down');
+$pdf->ln(2);
+
+// print misc.
+foreach($data->sections as $section1){
+	$pdf->SetFont('San','B',12);
+	$pdf->MultiCell($content_width, 7,strtoupper($section1->title).':',0,'L');
+	if (isset($section1->list)){
+		// TODO: change from horizontal to veritical
+		$pdf->SetFont('PragatiNarrow','',10);
+		$length = count($section1->list);
+		$i = 1;
+		foreach($section1->list as $array){
+			$pdf->Cell($indent,4,chr(149),'',0,'R');
+			$pdf->Cell($column_width-$indent,4,$array,'',0,'L');
+			if($i==1){
+			} else if ($i == $length) {
+			} else if(($i%3) == 0) {
+				$pdf->ln(4.4);
+			}
+			$i++;
+		}
+		$pdf->ln(4);
+	} else {
+		foreach($section1->array as $section2){
+			$pdf->SetFont('PragatiNarrow','B',11);
+			li(1,' ', $section2->title,$section2->date);
+			$pdf->SetFont('PragatiNarrow','I',8.5);
+			li(1,' ', $section2->description);
+			$pdf->ln(0.4);
+			foreach($section2->array as $section3){
+				$pdf->SetFont('PragatiNarrow','B',10);
+				li(2,NULL, $section3->title);
+				foreach($section3->array as $section4){
+					$pdf->SetFont('PragatiNarrow','',10);
+					li(3,'—', $section4);
+				}
+				$pdf->ln(0.4);
 			}
 			$pdf->ln(0.4);
 		}
-		$pdf->ln(0.4);
-	}	
+	}
 }
-
 $pdf->Output();
 ?>

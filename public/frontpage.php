0245d10e5fb7f448f215e8ae01c7e4f7f524ca4e
hxtree <matthewheroux@gmail.com>
2020-07-26 17:56:23 -0400

    Update framework image

--- public/frontpage.php
+++ public/frontpage.php
@@ -8,17 +8,22 @@
     <arg type="bool" name="frontpage">false</arg>
 </header>
 
-<div class="hero-image w-100" style="background-image:url('/assets/images/dimension/1260x630/offset/0,-20/hero/oscar-fish.jpg');">
+<div class="hero-image w-100 mb-5" style="background-image:url('/assets/images/dimension/1260x630/offset/0,-20/hero/pattern-bg.jpg');">
     <div class="container h-100">
         <div class="row h-100 align-items-center">
             <div class="col-12 text-left text-light">
-                <h1 class="font-weight-light">Our new website is on its way</h1>
-                <p class="lead">Check back soon for updates.</p>
+                <h1 class="font-weight-light">We make software</h1>
+                <p class="lead">that breaks assumptions.</p>
             </div>
         </div>
     </div>
 </div>
 
+<div class="container">
+    <p>
+        We are building the framework behind this site, <a href="https://github.com/ouxsoft/Hoopless">Hoopless</a>.
+    </p>
+</div>
 
 <footer name="Standard"/>
 </body>

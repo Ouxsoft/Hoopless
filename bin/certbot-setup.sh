e8215e2af4bc016de8ed48af993bae1fae84ccad
hxtree <matthewheroux@gmail.com>
2020-08-03 23:38:08 -0400

    Add apache flag

--- bin/certbot-setup.sh
+++ bin/certbot-setup.sh
@@ -3,7 +3,9 @@ apt-get install -y software-properties-common
 add-apt-repository -y ppa:certbot/certbot
 apt update
 apt install -y certbot
-certbot certonly --manual -d *.ouxsoft.com -d ouxsoft.com \
+certbot certonly \
+--apache \
+--manual -d *.ouxsoft.com -d ouxsoft.com \
 --agree-tos -m webmaster@ouxsoft.com \
 --no-bootstrap --manual-public-ip-logging-ok --preferred-challenges dns-01 \
 --server https://acme-v02.api.letsencrypt.org/directory

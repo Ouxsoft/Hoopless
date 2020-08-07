35d1c6636b939f920e8448d92fef67c7cccd4456
hxtree <matthewheroux@gmail.com>
2020-08-07 19:42:45 -0400

    Creating installation scripts

---
+++ bin/install/webserver.sh
@@ -0,0 +1,18 @@
+#
+# install apps
+apt-get update;
+apt-get install -y \
+    git \
+    zip \
+    unzip \
+    apache2 \
+    php7.2 \
+    libapache2-mod-php7.2 \
+    php7.2-mysql \
+    php7.2-dom \
+    php7.2-json \
+    php7.2-gd \
+    php7.2-yaml \
+    php7.2-mbstring \
+    libxml2 curl
+

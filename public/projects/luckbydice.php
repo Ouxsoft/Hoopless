0c0dd0799a91f32763a5f233d8759d179ff9d95f
hxtree <matthewheroux@gmail.com>
2021-07-26 22:34:17 -0500

    Add LuckByDice widget

---
+++ public/projects/luckbydice.php
@@ -0,0 +1,49 @@
+<html lang="en">
+<head name="Standard">
+    <title>LuckByDice</title>
+</head>
+<body>
+
+<header name="Standard"/>
+
+<div class="container">
+    <div class="row">
+        <main name="Standard" class="editable">
+            <h1>LuckByDice</h1>
+            <blockquote class="blockquote">
+                <p class="mb-0">
+                    The problem with dice rollers is there is no sense of luck! How can you quantity something illusive
+                    as luck?
+                </p>
+            </blockquote>
+
+
+            <p>
+                This is a problem we originally developed a solution for in a video game we released in 2005.
+                It started as an excel spreadsheet, then a Lua script, then a C# library, and now we're supporting it
+                as a PHP library, called LuckByDice.
+
+                <b>Give it a try!</b>
+            </p>
+
+            <widget name="DiceRoll">
+                <arg name="notation"></arg>
+            </widget>
+
+        </main>
+
+        <nav name="SideMenu">
+            <nav name="QuickLinks" class="editable">
+                <arg name="menu" type="string">help</arg>
+                <a href="https://github.com/Ouxsoft/LuckByDice">
+                    LuckByDice
+                </a>
+            </nav>
+        </nav>
+    </div>
+</div>
+
+<footer name="Standard"/>
+</body>
+</html>
+

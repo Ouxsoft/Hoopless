ec1c54ec2c96b4f44aa5adc34d0e74ab4c114742
hxtree <matthewheroux@gmail.com>
2021-08-12 19:51:32 -0500

    Remove comments

--- config/cli-config.php
+++ config/cli-config.php
@@ -1,23 +1,4 @@
 <?php
-/*
-use Doctrine\ORM\Tools\Console\ConsoleRunner;
-
-use Doctrine\DBAL\DriverManager;
-use Doctrine\Migrations\Configuration\Migration\PhpFile;
-use Doctrine\Migrations\Configuration\Connection\ExistingConnection;
-use Doctrine\Migrations\DependencyFactory;
-
-$entityManager = '';
-
-require_once __DIR__ . '/../bootstrap.php';
-
-return ConsoleRunner::createHelperSet($entityManager);
-
-
-$conn = DriverManager::getConnection();
-
-return DependencyFactory::fromConnection($config, new ExistingConnection($conn));
-*/
 
 require 'vendor/autoload.php';
 

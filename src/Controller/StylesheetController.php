43fa95e731260a8aae5589412bc97679d612e01c
hxtree <matthewheroux@gmail.com>
2021-10-13 22:36:34 -0500

    Move sass compile from bin to controller

--- src/Controller/StylesheetController.php
+++ src/Controller/StylesheetController.php
@@ -2,16 +2,44 @@
 
 namespace App\Controller;
 
+use ScssPhp\ScssPhp\Exception\CompilerException;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use ScssPhp\ScssPhp\Compiler;
+use ScssPhp\ScssPhp\Formatter\Compressed;
 
 class StylesheetController
 {
-    /**
-     * @Route("/assets/css/{file}", name="app_lucky_number")
-     */
-    public function get() : Response
-    {
+    const SCSS_DIR = __DIR__ . '/../../assets/scss/';
+    const BS_FRAMEWORK_DIR = __DIR__ . '/../../vendor/twbs/bootstrap/scss/';
+    const MAIN_CSS_DIR = __DIR__ . '/../../public/assets/css/main.min.css';
 
+    public function __construct()
+    {
+        // TODO: Add ACL
+    }
+
+    /**
+     * @Route("/backend/stylesheet", priority=2, name="stylesheetCompile")
+     * @return Response
+     * @throws CompilerException
+     */
+    public function compile() : Response
+    {
+        try {
+            $scss = new Compiler();
+            $scss->setFormatter(Compressed::class);
+            $scss->setImportPaths(self::SCSS_DIR);
+            $scss->addImportPath(self::BS_FRAMEWORK_DIR);
+
+            $output = $scss->compile('@import "main.scss";');
+
+            file_put_contents(self::MAIN_CSS_DIR, $output);
+
+            return new Response('Successfully compile stylesheets.');
+
+        } catch (Exception $e) {
+            return new Response('Unable to compile stylesheets.');
+        }
     }
 }

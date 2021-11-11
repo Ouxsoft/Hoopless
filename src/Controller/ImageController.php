3dca1773cbb806c4b926af46da7609d329b5e96b
hxtree <matthewheroux@gmail.com>
2021-11-11 00:20:31 -0600

    Move asset generation to package

--- src/Controller/ImageController.php
+++ src/Controller/ImageController.php
@@ -2,8 +2,8 @@
 
 namespace App\Controller;
 
-use App\Model\Image;
-use Psr\Log\LoggerInterface;
+use Ouxsoft\DynamoImage\DynamoImage;
+use Exception;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
@@ -14,56 +14,38 @@ class ImageController
      * @Route("/assets/images/{url}", priority=10, name="imageRoute", requirements={"url"=".+"})
      * @param $url
      * @return Response
+     * @throws Exception
      */
     public function indexAction($url) : Response
     {
-        $response = new Response();
 
-        $image = new Image();
-        $image->loadByURL("/assets/images/{$url}");
+        $image = new DynamoImage();
+        $image->setCacheDir(__DIR__ . '/../../var/cache/images/');
+        $image->setAssetDir(__DIR__ . '/../../public/assets/images/');
+        $image->setURL($url);
 
-
-        // send cached image
-//        if ($resource == $this->cache_filepath) {
-//            $this->sendCached();
-//        }
-
-
-        // load and resize file
-        if (! $image->load() || ! $image->resize()) {
-            // catch throws instead
-            $response->setStatusCode(404);
+        // send cache file if exists
+        if ($image->isCached()) {
+            $response = new BinaryFileResponse($image->getCacheFilepath());
+            $response->headers->set('Content-Length', $image->getFileSize());
+            $response->headers->set('Content-Type', $image->getContentType());
             return $response;
         }
 
-        $response->headers->set('Cache-Control', 'private');
+        // try to generate cache and send
+        try {
+            $image->resize();
+            $image->saveCache();
 
-        switch ($image->getFileExtension()){
-            case 'gif':
-                $response->headers->set('Content-Type', 'image/gif');
-                $response->sendHeaders();
-                $response->setContent(
-                    imagegif($image->image, null)
-                );
-                break;
-            case 'png':
-                $response->headers->set('Content-Type', 'image/png');
-                $response->sendHeaders();
-                $response->setContent(
-                    imagepng($image->image, null, 9)
-                );
-                break;
-            case 'jpeg':
-            case 'jpg':
-                $response->headers->set('Content-Type', 'image/jpeg');
-                $response->sendHeaders();
-                $response->setContent(
-                    imagejpeg($image->image, null, 100)
-                );
-                break;
-            default:
+            $response = new Response();
+            $response->headers->set('Cache-Control', 'private');
+            $response->headers->set('Content-Type', $image->getContentType());
+            $response->sendHeaders();
+            $response->setContent($image->getContent());
+            return $response;
+
+        } catch (Exception $e) {
+            return new Response('Resizing image failed');
         }
-
-        return $response;
     }
 }

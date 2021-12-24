ebdc2edd19ef976709154f4ebb34cada0e51dc91
hxtree <matthewheroux@gmail.com>
2021-12-24 03:03:01 -0600

    Dynamic footer menu

--- src/Element/Partial/PageFooter.php
+++ src/Element/Partial/PageFooter.php
@@ -17,11 +17,30 @@ use Ouxsoft\PHPMarkup\Element\AbstractElement;
  */
 class PageFooter extends AbstractElement
 {
+    private $menu;
+
+    public function onLoad()
+    {
+        $menu_id = 3;
+        $this->menu = $this->em->getConnection()->fetchAllAssociative(
+            '
+            SELECT 
+                COALESCE(`menu_item`.`title`, `page`.`title`) as `title`,
+                IF(`menu_item`.`page_id` IS NULL, `menu_item`.`url`, `page`.`url`) AS `url`
+            FROM `menu_item`
+            LEFT JOIN `page` ON `menu_item`.`page_id` = `page`.`page_id`
+            WHERE `menu_id` = ?
+            ORDER BY `order`',
+            [$menu_id]
+        );
+    }
+
     public function onRender()
     {
         return $this->view->render('/page-footer.html.twig', [
             'site_name' =>  $_ENV['SITE_NAME'],
-            'year' => date('Y')
+            'year' => date('Y'),
+            'menu' => $this->menu
         ]);
     }
 }
--- src/Migrations/Version20211224033039.php
+++ src/Migrations/Version20211224033039.php
@@ -21,6 +21,10 @@ final class Version20211224033039 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE menu_item ADD title VARCHAR(255) DEFAULT NULL');
+        $this->addSql("INSERT INTO `menu` (`menu_id`, `name`, `created`, `updated`) VALUES (NULL, 'Site Footer', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)");
+        $this->addSql("INSERT INTO `page` (`page_id`, `page_parent_id`, `title`, `url`, `keywords`, `template`, `created`, `updated`) VALUES (NULL, '1', 'Backend', '/backend', NULL, NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)");
+        $this->addSql("INSERT INTO `page` (`page_id`, `page_parent_id`, `title`, `url`, `keywords`, `template`, `created`, `updated`) VALUES (NULL, '23', 'Login', '/backend/login', NULL, NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)");
+        $this->addSql("INSERT INTO `menu_item` (`menu_item_id`, `menu_id`, `parent_menu_item_id`, `page_id`, `url`, `order`, `created`, `updated`, `title`) VALUES (NULL, '3', NULL, '3', NULL, NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL), (NULL, '3', NULL, '24', NULL, NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL)");
     }
 
     public function down(Schema $schema): void
